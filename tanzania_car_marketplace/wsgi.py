f18d97fa12aaade5d97ca25e010225b3b83794e8
mtui <mswimamtui0@gmail.com>
2026-06-19 03:36:28 +0300

    Fix wsgi.py application variable

--- tanzania_car_marketplace/wsgi.py
+++ tanzania_car_marketplace/wsgi.py
@@ -1,4 +1,6 @@
-"import os" 
-"from django.core.wsgi import get_wsgi_application" 
-"os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tanzania_car_marketplace.settings')" 
-"application = get_wsgi_application()" 
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tanzania_car_marketplace.settings')
+
+application = get_wsgi_application()
