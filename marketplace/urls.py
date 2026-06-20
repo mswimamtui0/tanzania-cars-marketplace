dace1c898e16fd698ee72d8a3ab5980985f51d21
mtui <mswimamtui0@gmail.com>
2026-06-20 12:18:48 +0300

    Fix: Add missing 'include' import in marketplace/urls.py

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from django.urls import path, include
 
 urlpatterns = [
     path('', views.home, name='home'),
