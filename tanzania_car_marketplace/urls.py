8ceec18b756ef587a02a88cb1a47006b46154997
mtui <mswimamtui0@gmail.com>
2026-06-20 12:54:28 +0300

    Remove allauth URL reference

--- tanzania_car_marketplace/urls.py
+++ tanzania_car_marketplace/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
-    path('accounts/', include('allauth.urls')),
+    #path('accounts/', include('allauth.urls')),
     path('', include('marketplace.urls')),
 ]
 
